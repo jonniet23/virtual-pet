931299e52790c84bc866ed0b27dab0befee8b2f4
Jonnie <jonniet23@hotmail.com>
2023-05-11 18:52:05 +0100

    Fitness and hunger properties added

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -11,6 +11,19 @@ describe('constructor', () => {
   
       expect(pet.age).toEqual(0);
     });
+
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+
+      expect(pet.hunger).toEqual(0);
+    });
+
+    it('has an initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+
+      expect(pet.fitness).toEqual(10);
+    });
+
     describe('growUp', () => {
       it('increments the age by 1', () => {
         const pet = new Pet('Fido');
@@ -18,6 +31,22 @@ describe('constructor', () => {
         pet.growUp();
     
         expect(pet.age).toEqual(1);
-      });
-  });
+        });
+     
+      it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.hunger).toEqual(5);
+        });
+
+        it('decreases the fitness by 3', () => {
+          const pet = new Pet('Fido');
+      
+          pet.growUp();
+      
+          expect(pet.fitness).toEqual(7);
+          });
+    });
 });
