dbb85f77aee27a71348452c47c88f464c9b00791
Jonnie <jonniet23@hotmail.com>
2023-05-12 10:17:03 +0100

    added walk function and refactoring

--- src/pet.js
+++ src/pet.js
@@ -1,13 +1,23 @@
+const MAXIMUM_FITNESS = 10;
+const FITNESS_EFFECT_FROM_WALK = 4;
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
-    this.fitness = 10;
+    this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
+};
+Pet.prototype.walk = function() {
+  if ((this.fitness + FITNESS_EFFECT_FROM_WALK) <= MAXIMUM_FITNESS) {
+    this.fitness += FITNESS_EFFECT_FROM_WALK;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
+    
 };
 module.exports = Pet;
